84ffa3146c3566e35e2edcf98aed0663a91ecb08
MontserratGonzalez1 <monkygs@gmail.com>
2016-10-10 13:05:14 -0500

    evaluación con JS

---
+++ ejercicio1.js
@@ -0,0 +1,26 @@
+var juego= prompt ("Escoge: piedra, papel o tijera"); {
+	if (i=1) {
+		juego= var tijera;
+	} else if {
+		juego = var papel;
+	} else {
+		juego = var piedra;
+	}
+	i = i +1;
+
+	console.log ("gana piedra") 
+
+}
+
+
+/* PSEUDOCODIGO 
+Inicio ;
+prompt ("Ingresa piedra, papel o tijera");
+Leer;
+for (i=piedra)
+if {i=piedra, i > papel, i++) {
+	console.log ("gana tijera");
+}
+
+*/
+
---
+++ ejercicio2.js
@@ -0,0 +1,23 @@
+
+var nombres = prompt ("Ingresa un nombre");
+var lista = n1+n2+n3;
+var suma = 0;
+
+for (var i=0, i < 3, i=i+1) {
+	suma= suma+ lista
+}
+
+alert(nombres);
+
+/* PSEUDOCODIGO 
+Inicio;
+prompt ("Ingresa un nombre 1 de 3");
+guardar en n1; 
+prompt ("Ingresa un nombre 2 de 3",);
+guardar en n2: 
+prompt ("Ingresa un nombre 3 de 3",);
+guardar en n3;
+Leer n1, n2,n3;
+alert mostrar nombre ("Los nombres ingresados son:" n1,n2, n3)
+Salir.
+*/
